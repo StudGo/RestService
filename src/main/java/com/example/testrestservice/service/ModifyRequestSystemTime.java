616c6870bf3037299114c13ff97d2b3fee1afb82
roman <j-kio@mail.ru>
2023-05-03 22:23:39 +0500

    lab15 part2 modify

--- src/main/java/com/example/testrestservice/service/ModifyRequestSystemTime.java
+++ src/main/java/com/example/testrestservice/service/ModifyRequestSystemTime.java
@@ -7,13 +7,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Service
 public class ModifyRequestSystemTime implements ModifyRequestService{
     @Override
     public void modifyRq(Request request){
-        request.setSystemTime(LocalDate.now().toString()); //Установить текущее время и преобразовать в строку
+        request.setSystemTime(LocalTime.now().toString()); //Установить текущее время и преобразовать в строку
         HttpEntity<Request> httpEntity = new HttpEntity<>(request);
 
         new RestTemplate().exchange("http://localhost:8082/feedback",
